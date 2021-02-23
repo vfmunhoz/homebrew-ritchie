1856c27274330caaef54e64cfdc3db0e69ffcb9e
Vinicius Montes Munhoz <viniciusfmunhoz@gmail.com>
2021-02-23 19:13:31 -0300

    fix: def caveats

--- Casks/ritchie.rb
+++ Casks/ritchie.rb
@@ -2,8 +2,10 @@ class Ritchie < Formula
     desc "CLI tool for writing your own automations"
     homepage "https://github.com/ZupIT/ritchie-cli"
 
-    caveats <<~EOS
-        Installing Ritchie client on your local system.
-    EOS
+    def caveats 
+        s = <<~EOS
+            Installing Ritchie client on your local system.
+        EOS
+    end
 
 end
