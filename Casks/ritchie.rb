e546644fa9a1891dfc98b4f08c4acc31801cafdf
Vinicius Montes Munhoz <viniciusfmunhoz@gmail.com>
2021-02-23 19:27:15 -0300

    feat: add Ritchie sh file

--- Casks/ritchie.rb
+++ Casks/ritchie.rb
@@ -1,17 +1,11 @@
 class Ritchie < Formula
     desc "CLI tool for writing your own automations"
     homepage "https://github.com/ZupIT/ritchie-cli"
+    url "https://commons-repo.ritchiecli.io/2.9.1/darwin/rit"
 
     def install
-        s = <<~EOS
-            Installing RitchieCLI
-        EOS
-    end
-
-    def caveats 
-        s = <<~EOS
-            Installing Ritchie client on your local system.
-        EOS
+        chmod 0755, "install.sh"
+        libexec.install %w[install.sh]
     end
 
 end
