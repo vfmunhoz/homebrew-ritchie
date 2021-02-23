df1fc34880755eed090b3f35a7f700e5724286e4
Vinicius Montes Munhoz <viniciusfmunhoz@gmail.com>
2021-02-23 19:04:13 -0300

    feat: implements Formula

--- Casks/ritchie.rb
+++ Casks/ritchie.rb
@@ -1,5 +1,7 @@
-cask 'ritchie' do
-    
+class Ritchie < Formula
+    desc "CLI tool for writing your own automations"
+    homepage "https://github.com/ZupIT/ritchie-cli"
+
     caveats <<~EOS
         Installing Ritchie client on your local system.
     EOS
