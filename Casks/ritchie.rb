668d580ddd42a17064f3ab97dd3d918fbaeb24dc
Vinicius Montes Munhoz <viniciusfmunhoz@gmail.com>
2021-02-23 19:32:12 -0300

    feat: add checksum

--- Casks/ritchie.rb
+++ Casks/ritchie.rb
@@ -2,6 +2,8 @@ class Ritchie < Formula
     desc "CLI tool for writing your own automations"
     homepage "https://github.com/ZupIT/ritchie-cli"
     url "https://commons-repo.ritchiecli.io/2.9.1/darwin/rit"
+    version "2.9.1"
+    sha256 "e5fa7fbc0a23ba8e33d10689c9b00b27af1e88fd3aefe6a5e4cce07471e42d52"
 
     def install
         chmod 0755, "install.sh"
