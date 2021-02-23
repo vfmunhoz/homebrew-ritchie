773f63f4eb8f574b47689f4e9d22f3c6727818da
Vinicius Montes Munhoz <viniciusfmunhoz@gmail.com>
2021-02-23 19:16:18 -0300

    feat: add install definition

--- Casks/ritchie.rb
+++ Casks/ritchie.rb
@@ -2,6 +2,12 @@ class Ritchie < Formula
     desc "CLI tool for writing your own automations"
     homepage "https://github.com/ZupIT/ritchie-cli"
 
+    def install
+        s = <<~EOS
+            Installing RitchieCLI
+        EOS
+    end
+
     def caveats 
         s = <<~EOS
             Installing Ritchie client on your local system.
