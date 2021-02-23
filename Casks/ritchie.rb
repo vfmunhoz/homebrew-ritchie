d8436224ad532cf59624fef6bc9bc58204adedda
Vinicius Montes Munhoz <viniciusfmunhoz@gmail.com>
2021-02-23 19:33:43 -0300

    fix: fix install.sh file address

--- Casks/ritchie.rb
+++ Casks/ritchie.rb
@@ -1,7 +1,7 @@
 class Ritchie < Formula
     desc "CLI tool for writing your own automations"
     homepage "https://github.com/ZupIT/ritchie-cli"
-    url "https://commons-repo.ritchiecli.io/2.9.1/darwin/rit"
+    url "https://commons-repo.ritchiecli.io/install.sh"
     version "2.9.1"
     sha256 "e5fa7fbc0a23ba8e33d10689c9b00b27af1e88fd3aefe6a5e4cce07471e42d52"
 
