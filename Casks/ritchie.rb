ac6873d39cfac6d1bd887aaff40f73fdac169223
Vinicius Montes Munhoz <viniciusfmunhoz@gmail.com>
2021-03-01 21:36:56 -0300

    change def

--- Casks/ritchie.rb
+++ Casks/ritchie.rb
@@ -1,9 +1,11 @@
-class Ritchie < Formula
+class RitchieTool < Formula
     desc "CLI tool for writing your own automations"
     homepage "https://github.com/ZupIT/ritchie-cli"
     url "https://commons-repo.ritchiecli.io/install.sh"
     version "2.9.1"
     sha256 "e5fa7fbc0a23ba8e33d10689c9b00b27af1e88fd3aefe6a5e4cce07471e42d52"
+
+    bottle :unneeded
 
     def install
         chmod 0755, "install.sh"
